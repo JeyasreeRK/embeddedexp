83445b5611fa47abf177a5db0d2ad8b9e2d956ca
Jeya Sree Krishnamoorthy <115264091+JeyasreeRK@users.noreply.github.com>
2023-06-20 18:19:11 +0530

    8 bit OUTPUT in Seq.c

--- 8 bit OUTPUT in Seq.c
+++ 8 bit OUTPUT in Seq.c
@@ -1,3 +1,5 @@
+                      METHOD 1(normal)
+
 #include <At89s52.h>
 void time(int);
 void main()
@@ -9,6 +11,33 @@ time(1);
 P0=0x55;
 time(1);
 }
+}
+void time(int del)
+{
+int c1,c2;
+for(c1=0;c1<del;c1++)
+{
+for(c2=0;c2<15;c2++)
+{
+}
+}
+}
+
+       METHOD 2(Using shift)
+
+#include <At89s52.h>
+void time(int);
+void main()
+{
+char d=0xAA;
+while(1){
+P0=d;
+time(1);
+P0=d<<1;
+time(1);
+}
+
+
 }
 void time(int del)
 {
