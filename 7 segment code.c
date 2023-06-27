0f7e57398e3f01747b1e1089a5af1326504e83c7
Jeya Sree Krishnamoorthy <115264091+JeyasreeRK@users.noreply.github.com>
2023-06-27 10:35:30 +0530

    7 segment code.c

--- 7 segment code.c
+++ 7 segment code.c
@@ -1,7 +1,18 @@
+COMMON ANODE
+
+
 #include <At89s52.h>
 #define sev P0
 void main(){
   sev=0xF8;
+  while(1){
+  }
+ }
+   COMMON CATHODE
+#include <At89s52.h>
+#define sev P0
+void main(){
+  sev=!0xF8;
   while(1){
   }
  }
