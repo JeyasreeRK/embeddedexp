4918c1e509dc0ba57403fbd289f97c951fc3aae9
Jeya Sree Krishnamoorthy <115264091+JeyasreeRK@users.noreply.github.com>
2023-07-06 10:17:25 +0530

    multiplexer 7 segment.c

--- multiplexer 7 segment.c
+++ multiplexer 7 segment.c
@@ -1,25 +1,28 @@
 #include <At89s52.h>
 #define sev P0
-#define dis P1
+#define dig P1
+#define b 0xFF
 const char c[11]={0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0x80,0x90};
+const char d[5]={0x0E,0x0D,0x0B,0x07};
 void time(int);
 void main(){
-sev=c[2];
-dis=0x07;
-time(4);
-sev=c[0];
-dis=0x0B;
-time(4);
-sev=c[2];
-dis=0x0D;
-time(4);
-sev=c[3];
-dis=0x0E;
-time(4);
+
 
 while(1)
 {
-
+sev=c[2];
+dig=d[3];
+dig=b;
+sev=c[0];
+dig=d[2];
+dig=b;
+sev=c[2];
+dig=d[1];
+dig=b;
+sev=c[3];
+dig=d[0];
+dig=b;
+break;
 }
 }
 void time(int del)
